81bf963d45c6c7b2ce87280d88aa29b87c8ef8fc
thesanju <sanjayjr1official@gmail.com>
2024-10-20 22:45:24 +0530

    unsaved commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,9 +20,7 @@ const Projects = () => {
 
 const ProjectCard = () => {
   return (
-    <Card
-    isFooterBlurred 
-    className="w-full">
+    <Card isFooterBlurred className="w-full">
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
         <p className="text-tiny uppercase font-bold">Stack used</p>
         <small className="text-default-500">Status</small>
@@ -38,10 +36,22 @@ const ProjectCard = () => {
       </CardBody>
       <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
         <p className="text-tiny text-white/80">Available soon.</p>
-        <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+        <Button
+          className="text-tiny text-white bg-black/20"
+          variant="flat"
+          color="default"
+          radius="lg"
+          size="sm"
+        >
           Code
         </Button>
-        <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+        <Button
+          className="text-tiny text-white bg-black/20"
+          variant="flat"
+          color="default"
+          radius="lg"
+          size="sm"
+        >
           Live
         </Button>
       </CardFooter>
